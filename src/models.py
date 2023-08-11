169479c4c1d878dd3e490df7fe1ceb19d07e1f6b
jeankory500 <124745650+jeankory500@users.noreply.github.com>
2023-08-11 02:14:35 +0000

    modelSave

--- src/models.py
+++ src/models.py
@@ -6,7 +6,28 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+            # do not serialize the password, its a security breach
+        }
+
+class Fav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    people_id = db.relationship("People")
+    planets_id = db.relationship("Planets")
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique= True, nullable=False)
+    eye_color = db.Column(db.String(40), unique= False)
+    people_fav = db.Column(db.Integer, db.ForeignKey(Fav.id))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +35,26 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "people_fav": self.people_fav
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique= True, nullable=False)
+    terrain = db.Column(db.String(40), unique= False)
+    planets_fav = db.Column(db.Integer, db.ForeignKey(Fav.id))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "planets_fav": self.planets_fav
             # do not serialize the password, its a security breach
         }
